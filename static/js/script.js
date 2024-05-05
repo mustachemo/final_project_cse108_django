ac145808d6201cfe83e8576f88941bf235f7f523
Amehroke <arashdeepsingh123@gmail.com>
2024-05-05 16:33:03 -0700

    Added new study room button, added url route, created view to render html, added leaflet script & link to main.html, rendered leaflet map to studyroom.html

--- static/js/script.js
+++ static/js/script.js
@@ -67,3 +67,12 @@ if (photoInput)
 // Scroll to Bottom
 const conversationThread = document.querySelector(".room__box");
 if (conversationThread) conversationThread.scrollTop = conversationThread.scrollHeight;
+
+document.addEventListener('DOMContentLoaded', function () {
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+});

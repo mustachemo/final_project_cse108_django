ac893f03d8828c7ddc5ec37da0e2feaabc82f541
Amehroke <arashdeepsingh123@gmail.com>
2024-05-05 18:58:08 -0700

    Added Map points for study rooms with css

--- static/js/script.js
+++ static/js/script.js
@@ -69,12 +69,46 @@ const conversationThread = document.querySelector(".room__box");
 if (conversationThread) conversationThread.scrollTop = conversationThread.scrollHeight;
 
 document.addEventListener('DOMContentLoaded', function () {
-  var map = L.map('map').setView([37.365688, -120.432228], 16);
-  map.panBy([300, 50]);
+  var map = L.map('map', {
+      center: [37.364333, -120.425583], // Center of the map
+      zoom: 16,
+      maxBounds: L.latLngBounds(
+          L.latLng(37.354333, -120.435583), // Southwest corner of the boundary
+          L.latLng(37.374333, -120.415583)  // Northeast corner of the boundary
+      ),
+      maxBoundsViscosity: 1.0, // Makes the bounds fully solid
+      zoomControl: true, // Enable zoom control buttons
+      attributionControl: true // Enable attribution control
+  });
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
+  // Create a home button
+  L.easyButton('fa-home', function(btn, map){
+      map.setView([37.364333, -120.425583], 16);
+  }, 'Go to Default View').addTo(map);
+
+  // Custom icon setup
+  var customIcon = L.icon({
+    iconUrl: '/static/images/map-marker.png', // Replace with your image path
+    iconSize: [50, 50], // size of the icon
+    iconAnchor: [25, 110], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -95] // point from which the popup should open relative to the iconAnchor
+  });
+
+  // Array of marker data
+  var locations = [
+      [37.364333, -120.425583, "Location 1"],
+      [37.365333, -120.426583, "Location 2"],
+      [37.363333, -120.424583, "Location 3"]
+  ];
+
+  // Adding markers to the map
+  locations.forEach(function(location) {
+      var marker = L.marker([location[0], location[1]], {icon: customIcon}).addTo(map);
+      marker.bindPopup("<b>" + location[2] + "</b>").openPopup();
+  });
 });
